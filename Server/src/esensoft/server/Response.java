b2d6b96929659f436e00284f9c71de4249214202
deng chao <deng.369@gmail.com>
2011-08-03 10:57:49 +0800

    一，搞定了自动测试会报错，说classpath不对的问题。二，读取报文头的时候，使用的流把bufferedReader改成了inputsream，并修复了它在读取的时候会卡住的问题。三，把原来读取报文头，截取url的两个函数合并成了一个函数。

---
+++ Server/src/esensoft/server/Response.java
@@ -0,0 +1,221 @@
+package esensoft.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *All right resrvered esensoft(2011)
+ * @author  邓超   deng.369@gmail.com
+ * @version 1.0,创建时间：2011-8-2 下午07:12:36
+ * @since   jdk1.5
+ * 类说明
+ */
+public class Response implements Runnable {
+	/*
+	 * 这个socket对象必须要从Serve监听对象传过来，和浏览器交互的IO流都是通过它来建立，
+	 * 通过这个对象的输入流来获取url，通过这个对象的输出流来向浏览器返回内容。 不可缺少，必须要有
+	 */
+	private Socket socket;
+	/* 服务器的默认根目录，从Serve监听对象中获得 ，可以缺省 */
+	private String root;
+	/* 文件的目录 */
+	private String sourceName;
+    
+    private Logger logger = LoggerFactory.getLogger(Response.class);
+	public Response(Socket response, String root) {
+		this.socket = response;
+		this.root = root;
+	} 
+	/**
+	 * 这个是系统盘，非常反对将根目录放在这里
+	 * */
+	public Response(Socket response) {
+		this(response, "c:\\");
+	}
+
+	/**
+	 * 核心方法，规定该线程的执行内容，通过浏览器的输入流得到请求报文头，从中截取文件Url,
+	 * 如果文件存在，读取文件，将内容传输到浏览器。不存在，返回404文件
+	 * 
+	 * @exception IOException
+	 * */
+	public void run() {
+		
+			OutputStream out = null;
+			InputStream socketiStream = null;
+			try {
+				socketiStream = socket.getInputStream();
+				/*用这个方法获得的名字，有可能不是用户输入的url地址。尤其是在用户输入多个
+				 * %或者@号的时候，你就获取不到这些东西了！*/
+				sourceName = getUrlFromStream(socketiStream);
+				try {
+					out = socket.getOutputStream();
+					fileToBrowser(sourceName, out);
+				} catch (IOException e) {
+					logger.error("输出流对象新建的时候出错！程序终止！,");
+				}
+			} catch (IOException e) {
+				logger.error("程序运行中出现错误，程序终止！");
+			
+			} finally {
+				try {
+					out.close();
+					socketiStream.close();
+					socket.close();
+				} catch (IOException e) {
+					logger.error("文件流在关闭的时候出现错误！，不能正常关闭");
+				}
+			}
+		
+	}
+
+	/**
+	 * 从socket中的输入流，获取请求报文所有内容
+	 * 
+	 * @param BufferedReader
+	 *            socket获得的输入流，由它获取请求报文
+	 * @exception IOException
+	 * */
+	public String getUrlFromStream(InputStream inputStream) {
+		StringBuilder builder = new StringBuilder();
+		try {
+			int len=-1;
+			byte[] buff =new byte[1024];
+			do{
+				len=inputStream.read(buff);
+				if(len!=-1)
+				{
+					builder.append(new String(buff,0,len,"UTF-8")).append('\n');
+				}
+			}while(len==1024);
+			
+		} catch (IOException e) {
+			logger.error("读取请求报文过程中出错！");
+		
+		}
+		String requestString = builder.toString();
+		logger.info("请求的报文头是：{}", requestString);
+		if ("".equals(requestString))
+			return "";
+		else {
+			/*
+			 * 截取报文中从第五个开始到“HTTP/1.1”之间的所有字符，因为请求报文第一行的格式一般都是 
+			 * GET /urlcontent HTTP/1.1
+			 */
+			requestString = requestString.substring(5,
+					requestString.lastIndexOf("HTTP/1.1") - 1);
+			/* 如果对方输入的页面是空的，即输入的就是服务器的地址，例如http://localhost:8080,那么就跳至默认首页,否则就是查找文件 */
+			if ("".equals(requestString))
+				{
+				requestString = "index.html";
+				return requestString;
+				}
+			/*空格经过浏览器UTF-8编码处理后会变成%20传过来*/
+			requestString =requestString.replaceAll("%20"," ");
+			logger.info("文件名是：{}", requestString);
+			return requestString;
+		}
+	}
+
+	/**
+	 * 读取文件，写入浏览器 如果组装好的url指定的文件存在，就读取文件，传到浏览器。如果不存在，就在浏览器上显示404页面
+	 * 这里可能出现的问题：如果用户指定的404页面不存在， 那么程序就会陷入死循环。
+	 * InputStream会抛出FileNotFoundException异常，is.read(buf)会抛出IOException
+	 * 
+	 * @param sourceName
+	 *            ，要读取文件的文件名
+	 * @param out
+	 *            将文件写入浏览器的输出流
+	 * @throws IOException
+	 *             ， FileNotFoundException
+	 * */
+	public void fileToBrowser(String sourceName, OutputStream out)
+			throws FileNotFoundException, IOException {
+		File file = new File(root, sourceName);
+		if (file.exists()&&file.isFile()) {
+			InputStream is = null;
+			try {
+				
+				/*
+				 * 自定义的http应答报文的报文头，先把他发给浏览器。格一个空行后， 在这个后面加入要传给浏览器的文件内容
+				 */
+				String head = "HTTP/1.1 200 OK" + "\n"
+						+ "Date: Thu, 21 Jul 2011 01:45:42 GMT" + "\n"
+						+ "Content-Length: " + file.length() + "\n"
+						+ "Content-Type: "+getMIMEtpye(sourceName) + "\n"
+						+ "Cache-Control: private" + "\n"
+						+ "Expires: Thu, 21 Jul 2011 01:45:42 GMT" + "\n"
+						+ "Connection: Keep-Alive" + "\n" + "\n";
+				out.write(head.getBytes());
+				byte[] buf = new byte[1024];
+				int len;
+				is = (InputStream) (new FileInputStream(file));
+				while ((len = is.read(buf)) != -1) {
+					out.write(buf, 0, len);
+				}
+				out.flush();
+			} catch (IOException e) {
+				/*这个异常没必要终止程序，读取写入文件错误，还是有可能把少量信息传到浏览器的*/
+				logger.error("程序要找的文件{}能找到，可是文件在读取和写入过程中出错！", sourceName);
+				e.printStackTrace();
+			} finally {
+				/* 可能无法正常关闭 */
+				try {
+					is.close();
+				} catch (IOException e2) {
+					logger.error("{}文件读取流无法正常关闭！", sourceName);
+			
+				}
+			}
+		} else {
+			logger.error("程序要找的文件{}找磁盘上找不到！将会用404页面来替代这个文件返回！", root+sourceName);
+			/*
+			 * 如果找不到指定的文件，就是资源不存在，就跳至404文件 注意这是递归调用，如果说这个指定的404文件不存在，就会陷入
+			 * 死循环，不停调用，这个函数也就会一直执行，没有出口
+			 */
+			fileToBrowser("404.html", out);
+		}
+	}
+
+	/**
+	 * 根据文件名的后缀类型来确定返回类型。因为执行这段代码之前， 已经知道这个文件肯定存在，所以肯定是有后缀的
+	 * 决定报文头“Content-Type”那一行的内容， 例如如果是aa.jpg，返回就是"Content-Type: image/jpeg"
+	 * 
+	 * @param String
+	 *            sourceString ，url的字符串
+	 */
+	public String getMIMEtpye(String sourceString) {
+		// TODO 待完善
+		String returnType;
+		/* 图片的返回类型 */
+		if (sourceString.endsWith(".jpg") || sourceString.endsWith(".jpeg"))
+			returnType = "image/jpeg";
+		else if (sourceString.endsWith(".jpg"))
+			returnType = "image/gif";
+		else if (sourceString.endsWith(".png"))
+			returnType = "image/png";
+		/* 文本文件的返回类型 */
+		else if (sourceString.endsWith(".xml"))
+			returnType = "text/xml";
+		else if (sourceString.endsWith(".txt") || sourceString.endsWith(".c")
+				|| sourceString.endsWith(".cpp")
+				|| sourceString.endsWith(".java")
+				|| sourceString.endsWith(".h"))
+			returnType = "text/plain";
+		else {
+			returnType = "text/html;charset=gb2312";
+		}
+		return returnType;
+	}
+}
+
+
---
+++ Server/test/esensoft/server/TestResponse.java
@@ -0,0 +1,33 @@
+package esensoft.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ *All right resrvered esensoft(2011)
+ * @author  邓超   deng.369@gmail.com
+ * @version 1.0,创建时间：2011-8-3 上午08:44:10
+ * @since   jdk1.5
+ * 对response类的测试类
+ */
+public class TestResponse {
+
+	@Test
+	public final void testGetUrlFromStream() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFileToBrowser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetMIMEtpye() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
+
+
